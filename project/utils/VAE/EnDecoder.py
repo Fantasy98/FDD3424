b776c8e06d9f5b65fa4fcc0875dc2509acbd3934
Yuning Wang <yuningw@mech.kth.se>
2024-05-15 08:44:05 +0200

    Update project

--- project/utils/VAE/EnDecoder.py
+++ project/utils/VAE/EnDecoder.py
@@ -132,6 +132,130 @@ class encoder(nn.Module):
         return (z_mean,z_var)
 
 
+
+###########################################
+# A fixed Encoder for vanilla AutoEncoder, which has one linear layer for compressing to the data
+###########################################
+class fix_encoder(nn.Module):
+    def __init__(self,
+                    zdim,
+                    knsize,
+                    filters     = [1,16,32,64,128,256],
+                    block_type  = "deep",
+                    lineardim    = 128, 
+                    act_conv    = "elu",
+                    act_linear   = "elu") -> None:
+        """
+        Module of encoder architecture used for vanilla AutoEncoder 
+        
+        Args:
+            zdim:   latent_dim
+            knsize: kernel size of Convolution layer
+            filters: A list of number of filters used in Convblock
+            block_type: The type of ConvBlock used in architecture 
+            lineardim: The dimension of linear layer 
+            act_conv : The activation fuction for convblock
+            act_linear : The activation fuction for linear layer
+
+        
+        """
+        super(encoder,self).__init__()
+        self.knsize = knsize
+        self.zdim = zdim
+
+        self.h, self.w, self.c = 96, 192, 1
+        
+        c_comp, h_comp, w_comp= self.compute_compression(filters[1:])   
+
+        if (int(h_comp) == h_comp) and (int(w_comp) == w_comp):
+            print(f"The stragtegy of compression is valid, the final size of domain is [{c_comp, h_comp, w_comp}], flatten = ({c_comp * h_comp * w_comp})")
+            
+        else:
+            print("The compression does not work!!")        
+
+        self.ConvBlocks = nn.Sequential()
+        for i, f in enumerate(filters[:-1]): 
+            if block_type  == "original":
+                self.ConvBlocks.add_module(
+                                        f"Down{i}",DownBlock(   in_channel  =   filters[i], 
+                                                                out_channel =   filters[i+1],
+                                                                knsize      =   knsize,
+                                                                activation  =   act_conv)
+                                        )
+            
+            if block_type  == "bn_original":
+                self.ConvBlocks.add_module(
+                                        f"Down{i}",BNDownBlock(   in_channel  =   filters[i], 
+                                                                out_channel =   filters[i+1],
+                                                                knsize      =   knsize,
+                                                                activation  =   act_conv)
+                                        )
+            
+            if block_type  == "deep":
+                self.ConvBlocks.add_module(
+                                        f"Down{i}",deepDownBlock(   in_channel  =   filters[i], 
+                                                                out_channel =   filters[i+1],
+                                                                knsize      =   knsize,
+                                                                activation  =   act_conv)
+                                        )
+            if block_type  == "res":
+                self.ConvBlocks.add_module(
+                                        f"Down{i}",ResDownBlock(   in_channel  =   filters[i], 
+                                                                out_channel =   filters[i+1],
+                                                                knsize      =   knsize,
+                                                                activation  =   act_conv)
+                                        )
+                
+        
+        
+        self.flat  = nn.Flatten()
+
+        
+        self.linear = nn.Linear(in_features= (int(h_comp) * int(w_comp) * int(c_comp)),
+                                out_features= lineardim)
+        
+        if act_linear =="elu":
+            self.act = nn.ELU()
+        if act_linear == "tanh":
+            self.act = nn.Tanh()
+
+        self.lin_mean = nn.Linear(in_features=lineardim,out_features=zdim)
+        
+        nn.init.xavier_uniform_(self.lin_mean.weight)
+        nn.init.xavier_uniform_(self.linear.weight)
+        nn.init.zeros_(self.lin_mean.bias)
+        nn.init.zeros_(self.linear.bias)
+    
+    def compute_compression(self, filters:list):
+        """
+        Compute the compression size of the input domain 
+        Args: 
+            self: Gives the channel, height and weight of domain 
+            filers: List of filter number 
+        
+        Returns:
+            (c_comp, h_comp, w_comp): [C, H, W] The size of domain after compression
+        """ 
+        Nostep  = len(filters) 
+        h_comp  = self.h * 0.5 ** Nostep
+        w_comp  = self.w * 0.5 ** Nostep
+        c_comp  = filters[-1]   
+
+        return (c_comp, h_comp, w_comp) 
+
+
+    
+    def forward(self,x):
+        x = self.ConvBlocks(x)
+        x = self.flat(x)
+        x = self.act(self.linear(x))
+        z_mean = self.lin_mean(x)
+        return z_mean
+
+############################################################################
+
+
+
 ###########################################
 ## Decoder Module
 ###########################################
